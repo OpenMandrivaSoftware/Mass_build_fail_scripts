6e8aeebb8f847615ad4b136b7afed62456ec888f
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2018-11-16 12:14:54 +0100

    add MASSBUILD_ID as variable

--- update-mass-build-status.sh
+++ update-mass-build-status.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 [ -e /etc/sysconfig/massbuild.conf ] && . /etc/sysconfig/massbuild.conf
 if [ -z "$ABFAUTH" ]; then
-	echo "Set ABFAUTH=YOUR_API_KEY: in /etc/sysconfig/massbuild.conf"
+	printf '%s\n' "Set ABFAUTH=YOUR_API_KEY: in /etc/sysconfig/massbuild.conf"
+	exit 1
+fi
+if [ -z "$MASSBUILD_ID" ]; then
+	printf '%s\n' "Set MASSBUILD_ID= in /etc/sysconfig/massbuild.conf to run scripts"
 	exit 1
 fi
 cd /opt/Mass_build_fail_scripts
 git pull
 if [ -e massbuild.lock ]; then
-	echo "Previous iteration still running" >&2
+	printf '%s\n'  "Previous iteration still running" >&2
 	exit 1
 fi
 touch massbuild.lock
 [ -e /home/omv/massbuild/massbuild-new.html ] && rm -rf /home/omv/massbuild/massbuild-new.html
 [ ! -e /home/omv/massbuild/massbuild-new.html ] && touch /home/omv/massbuild/massbuild-new.html && chown massbuild:massbuild /home/omv/massbuild/massbuild-new.html
 
-./generate-status.sh 182 >/home/omv/massbuild/massbuild-new.html
+. /etc/sysconfig/massbuild.conf
+./generate-status.sh "$MASSBUILD_ID" >/home/omv/massbuild/massbuild-new.html
 mv -f /home/omv/massbuild/massbuild-new.html /home/omv/massbuild/massbuild.html
 rm -f massbuild.lock
